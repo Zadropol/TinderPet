b8be1677a682d47a61bfe401d48e6b5b92458102
Alous12 <alejandro.machaca@ucb.edu.bo>
2025-10-21 22:39:42 -0400

    refactory: se muestra el nombre de la mascota en la tarjeta

--- cypress/e2e/perfilmascota.cy.js
+++ cypress/e2e/perfilmascota.cy.js
@@ -1,6 +1,8 @@
 describe("Crear Perfil", () => {
-  it("Deberia poder ingresar el nombre de la mascota", () => {
+  it("Deberia poder ingresar y mostrar el nombre de la mascota", () => {
     cy.visit("/");
     cy.get("#nombre-mascota").type("Firulais");
+    cy.get("#crear-button").click();
+    cy.get("#resultado-div").should("contain", "Firulais");
   });
 });
--- src/perfilmascota.spec.js
+++ src/perfilmascota.spec.js
@@ -1,7 +1,7 @@
 import crearperfil from "./perfilmascota";
 
 describe("Crear Perfil", () => {
-  it("deberia poder ingresar el nombre de la mascota", () => {
+  it("deberia poder ingresar y mostrar el nombre de la mascota", () => {
     expect(crearperfil("Firulais")).toEqual("Firulais");
   });
 });
