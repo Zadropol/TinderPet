0c2f11995e2941eee85c88a451b527864fe173c6
Adrian Rodriguez <aaadrianrodriguezmontecinos@gmail.com>
2025-10-22 18:48:47 -0400

    test: Se verifico la existenciade la funcion EnviarSolicitudAdoptar

---
+++ src/adopcion.js
@@ -0,0 +1,2 @@
+function EnviarSolicitudAdoptar() {}
+export { EnviarSolicitudAdoptar };
---
+++ src/adopcion.spec.js
@@ -0,0 +1,10 @@
+import { EnviarSolicitudAdoptar } from "./adopcion.js";
+
+describe( "EnviarSolicitudAdoptar", () => {
+
+  test("La funciona EnviarSolicitudAdoptar esta definida", () => {
+    expect(typeof EnviarSolicitudAdoptar ).toBe("function");
+  });
+
+} );
+    
